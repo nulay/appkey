9299efe3708c3dda3d120535c992f2afe912ec17
Mihail Kachanovskii <nulay@mail.ru>
2016-05-23 00:20:21 +0300

    chanfe project create new builder

--- razborImage/src/by/imix/razborImage/PanelToolsEmulation.java
+++ razborImage/src/by/imix/razborImage/PanelToolsEmulation.java
@@ -25,13 +25,13 @@ import java.util.Vector;
  * To change this template use File | Settings | File Templates.
  */
 public class PanelToolsEmulation extends JToolBar implements FocusListener, ActionListener, EventStopGKL {
-    private Logger _log=Logger.getLogger(PanelToolsEmulation.class);
-    private Screen4 screen4;
+    private Logger _log = Logger.getLogger(PanelToolsEmulation.class);
+    private GlobalFrame screen4;
 
     private JFileChooser openerFile;
     private JFileChooser saverFileemulation;
-
-//    private Vector l;//список названий эмуляций
+ 
+    //    private Vector l;//список названий эмуляций
     private Vector<ObKeyPressed> l2;//список объектов эмуляций
     private Vector<File> l3;//список файлов объектов эмуляций
     private JList listObj;
@@ -45,13 +45,13 @@ public class PanelToolsEmulation extends JToolBar implements FocusListener, Acti
 
     private DialogChKeyPr dialogCrEm;
 
-    public PanelToolsEmulation(Screen4 screen4) {
-        super("p1",JToolBar.VERTICAL);
+    public PanelToolsEmulation(GlobalFrame screen4) {
+        super("p1", JToolBar.VERTICAL);
         setAlignmentX(Component.LEFT_ALIGNMENT);
-        this.screen4=screen4;
-
-        JPanel rPanel=new JPanel();
-        rPanel.setLayout(new BoxLayout(rPanel,BoxLayout.Y_AXIS));
+        this.screen4 = screen4;
+      
+        JPanel rPanel = new JPanel();
+        rPanel.setLayout(new BoxLayout(rPanel, BoxLayout.Y_AXIS));
         rPanel.add(Box.createHorizontalGlue());
         setMinimumSize(new Dimension(220, 170));
         setMaximumSize(new Dimension(220, 170));
@@ -60,47 +60,47 @@ public class PanelToolsEmulation extends JToolBar implements FocusListener, Acti
 
         // listObPan.setBorder(OFF_BORDER);
 
-        JLabel lab=new JLabel(" Список эмуляций");
+        JLabel lab = new JLabel(" Список эмуляций");
         setAlignmentX(Component.LEFT_ALIGNMENT);
         rPanel.add(lab);
 
 //        l=new Vector();
-        l2=new Vector<ObKeyPressed>();
-        l3=new Vector<File>();
+        l2 = new Vector<ObKeyPressed>();
+        l3 = new Vector<File>();
 
-        listObj=new JList(l2);
+        listObj = new JList(l2);
 //        listObj.setMinimumSize(new Dimension(170, 120));
 //        listObj.setPreferredSize(new Dimension(170,120));
 //        listObj.setMaximumSize(new Dimension(170, 120));
 
         listObj.addFocusListener(this);
 
-        rPanel.add(new JScrollPane(listObj,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER));
+        rPanel.add(new JScrollPane(listObj, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER));
 
-        but4Play =new JButton(new ImageIcon("images/play.png"));//проигрывание
+        but4Play = new JButton(new ImageIcon("images/play.png"));//проигрывание
         but4Play.setName("but4Play");
         but4Play.setEnabled(false);
         but4Play.setBorder(screen4.OFF_BORDER);
-        but5Rec =new JButton(new ImageIcon("images/rec.png"));//запись
+        but5Rec = new JButton(new ImageIcon("images/rec.png"));//запись
         but5Rec.setName("but5Rec");
         but5Rec.setBorder(screen4.OFF_BORDER);
-        but6Folder=new JButton(new ImageIcon("images/folder.png"));
+        but6Folder = new JButton(new ImageIcon("images/folder.png"));
         but6Folder.setName("but6Folder");
         but6Folder.setBorder(screen4.OFF_BORDER);
-        but7Stop=new JButton(new ImageIcon("images/stop.png"));//стоп
+        but7Stop = new JButton(new ImageIcon("images/stop.png"));//стоп
         but7Stop.setName("but7Stop");
         but7Stop.setEnabled(false);
         but7Stop.setBorder(screen4.OFF_BORDER);
-        but8Correct=new JButton(new ImageIcon("images/redact.png"));
+        but8Correct = new JButton(new ImageIcon("images/redact.png"));
         but8Correct.setName("but8Correct");
         but8Correct.setBorder(screen4.OFF_BORDER);
         but8Correct.setEnabled(false);
-        but9delete=new JButton(new ImageIcon("images/delete.png"));
+        but9delete = new JButton(new ImageIcon("images/delete.png"));
         but9delete.setName("but9delete");
         but9delete.setBorder(screen4.OFF_BORDER);
         but9delete.setEnabled(false);
 
-        JPanel panforBut=new JPanel(new  FlowLayout());
+        JPanel panforBut = new JPanel(new FlowLayout());
         but7Stop.addActionListener(this);
         panforBut.add(but7Stop);
         but5Rec.addActionListener(this);
@@ -124,7 +124,7 @@ public class PanelToolsEmulation extends JToolBar implements FocusListener, Acti
 
     @Override
     public void focusGained(FocusEvent e) {
-        if(((JList)e.getSource()).getSelectedIndex()!=-1){
+        if (((JList) e.getSource()).getSelectedIndex() != -1) {
             showEmButton();
         }
     }
@@ -136,11 +136,11 @@ public class PanelToolsEmulation extends JToolBar implements FocusListener, Acti
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (((JButton)e.getSource()).getName().equals("but4Play")){
-            if (screen4.clss == null || screen4.clss.isKeyEnd()) {
+        if (((JButton) e.getSource()).getName().equals("but4Play")) {
+            if (screen4.getClss() == null || screen4.getClss().isKeyEnd()) {
                 int i = listObj.getSelectedIndex();
                 if (i == -1) {
-                    final JDialog dialog1 = new JDialog(screen4);
+                    final JDialog dialog1 = new JDialog((Frame) screen4);
                     dialog1.getContentPane().setLayout(new BoxLayout(dialog1.getContentPane(), BoxLayout.Y_AXIS));
 
                     dialog1.add(new JLabel("Не выбран бой"));
@@ -159,7 +159,7 @@ public class PanelToolsEmulation extends JToolBar implements FocusListener, Acti
                 }
 
                 but4Play.setBorder(screen4.ON_BORDER);
-                final JDialog dialog = new JDialog(screen4,"Проигрывание эмуляции");
+                final JDialog dialog = new JDialog((Frame) screen4, "Проигрывание эмуляции");
                 final JLabel l = new JLabel("Бой начнется через 3 секунды, переключитесь в игру");
                 dialog.add(l);
                 dialog.pack();
@@ -177,26 +177,26 @@ public class PanelToolsEmulation extends JToolBar implements FocusListener, Acti
                             _log.error(ez.getMessage());
                         }
                         dialog.setVisible(false);
-                        if (screen4.clss == null) {
-                            screen4.clss = new AppClss();
+                        if (screen4.getClss() == null) {
+                            screen4.setClss(new AppClss());
                         }
 
-                        screen4.clss.playBot();       //l2.get(listObj.getSelectedIndex()).getListKP()
+                        screen4.getClss().playBot();       //l2.get(listObj.getSelectedIndex()).getListKP()
                     }
                 });
                 th.start();
                 dialog.setVisible(true);
-                screen4.keySt=Screen4.PLAY;
+                screen4.setKeySt(GlobalFrame.PLAY);
                 but7Stop.setEnabled(true);
                 hideEmButton();
             }
         }
 
-        if(((JButton)e.getSource()).getName().equals("but5Rec")){
-            if (screen4.glklE == null || !screen4.glklE.isRun()) {
+        if (((JButton) e.getSource()).getName().equals("but5Rec")) {
+            if (screen4.getGlklE() == null || !screen4.getGlklE().isRun()) {
                 but5Rec.setBorder(screen4.ON_BORDER);
-                final JDialog dialog = new JDialog(screen4, "Старт записи");
-                final JLabel lab=new JLabel("Запсь начнется через 2 секунды, переключитесь в игру, для остановки и записи нажмите Escape.");
+                final JDialog dialog = new JDialog((Frame) screen4, "Старт записи");
+                final JLabel lab = new JLabel("Запсь начнется через 2 секунды, переключитесь в игру, для остановки и записи нажмите Escape.");
                 dialog.add(lab);
                 dialog.pack();
                 dialog.setSize(new Dimension(dialog.getWidth(), dialog.getHeight() + 20));
@@ -206,18 +206,18 @@ public class PanelToolsEmulation extends JToolBar implements FocusListener, Acti
                     @Override
                     public void run() {
                         try {
-                            for(int i=0;i<1;i++){
-                                lab.setText("Запсь начнется через "+(2-i)+" секунды, переключитесь в игру, для остановки и записи нажмите Escape.");
+                            for (int i = 0; i < 1; i++) {
+                                lab.setText("Запсь начнется через " + (2 - i) + " секунды, переключитесь в игру, для остановки и записи нажмите Escape.");
                                 Thread.sleep(1L * 1000L);
                             }
                             dialog.setVisible(false);
                         } catch (InterruptedException ez) {
                             ez.printStackTrace();
                         }
-                        if (screen4.glklE == null) {
-                            screen4.glklE = new GlobalKeyListenerExample(PanelToolsEmulation.this);
+                        if (screen4.getGlklE() == null) {
+                            screen4.setGlklE(new GlobalKeyListenerExample(PanelToolsEmulation.this));
                         } else {
-                            screen4.glklE.startKeyLovec();
+                            screen4.getGlklE().startKeyLovec();
                         }
 
                     }
@@ -225,13 +225,13 @@ public class PanelToolsEmulation extends JToolBar implements FocusListener, Acti
                 th.start();
                 dialog.setVisible(true);
                 but7Stop.setEnabled(true);
-                screen4.keySt=Screen4.REC;
-                listObj.removeSelectionInterval(0,listObj.getSelectedIndex());
+                screen4.setKeySt(GlobalFrame.REC);
+                listObj.removeSelectionInterval(0, listObj.getSelectedIndex());
                 hideEmButton();
             }
         }
 
-        if(((JButton)e.getSource()).getName().equals("but6Folder")){
+        if (((JButton) e.getSource()).getName().equals("but6Folder")) {
             if (openerFile == null) {
                 openerFile = new JFileChooser();
                 openerFile.setDialogTitle("Открытие эмуляции");
@@ -239,7 +239,7 @@ public class PanelToolsEmulation extends JToolBar implements FocusListener, Acti
                 openerFile.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 //                    openerFile.addChoosableFileFilter();
             }
-            int res = openerFile.showOpenDialog(screen4);
+            int res = openerFile.showOpenDialog((Component) screen4);
             if (res == JFileChooser.APPROVE_OPTION) {
                 File[] listF = openerFile.getSelectedFile().listFiles(new FilenameFilter() {
                     @Override
@@ -247,15 +247,15 @@ public class PanelToolsEmulation extends JToolBar implements FocusListener, Acti
                         return name.endsWith("emu");
                     }
                 });
-                if(l2.size()>0){
-                    int res2=JOptionPane.showConfirmDialog(screen4,"Заменить текущие эмуляции новыми?","Добавление эмуляций",JOptionPane.YES_NO_OPTION);
-                    if(res2==JOptionPane.YES_OPTION){
+                if (l2.size() > 0) {
+                    int res2 = JOptionPane.showConfirmDialog((Component) screen4, "Заменить текущие эмуляции новыми?", "Добавление эмуляций", JOptionPane.YES_NO_OPTION);
+                    if (res2 == JOptionPane.YES_OPTION) {
                         l2.clear();
                         l3.clear();
                     }
                 }
                 for (File f : listF) {
-                    ObKeyPressed okp = (ObKeyPressed) screen4.fo.readObjectFromFile(f);
+                    ObKeyPressed okp = (ObKeyPressed) screen4.getFileOperation().readObjectFromFile(f);
 //                    l.add((okp.getTitle() == null || okp.getTitle().equals("")) ? "NoName" : okp.getTitle());
                     l2.add(okp);
                     l3.add(f);
@@ -264,66 +264,66 @@ public class PanelToolsEmulation extends JToolBar implements FocusListener, Acti
             }
         }
 
-        if(((JButton)e.getSource()).getName().equals("but7Stop")){
-            if(screen4.keySt.equals(Screen4.PLAY)){
-                screen4.clss.setKeyEnd(true);
+        if (((JButton) e.getSource()).getName().equals("but7Stop")) {
+            if (screen4.getKeySt().equals(Screen4.PLAY)) {
+                screen4.getClss().setKeyEnd(true);
                 showEmButton();
                 but4Play.setBorder(but6Folder.getBorder());
             }
-            if(screen4.keySt.equals(Screen4.REC)){
+            if (screen4.getKeySt().equals(Screen4.REC)) {
                 but5Rec.setBorder(but6Folder.getBorder());
-                screen4.keySt=null;
-                screen4.glklE.stopKeyLovec();
+                screen4.setKeySt(null);
+                screen4.getGlklE().stopKeyLovec();
             }
-            screen4.keySt=null;
+            screen4.setKeySt(null);
             but7Stop.setEnabled(false);
         }
 
-        if(((JButton)e.getSource()).getName().equals("but8Correct")){
-            dialogCrEm=new DialogChKeyPr(screen4,l2.get(listObj.getSelectedIndex()));
+        if (((JButton) e.getSource()).getName().equals("but8Correct")) {
+            dialogCrEm = new DialogChKeyPr((Frame) screen4, l2.get(listObj.getSelectedIndex()));
             dialogCrEm.setVisible(true);
         }
 
-        if(((JButton)e.getSource()).getName().equals("but9delete")){
-            int res=JOptionPane.showConfirmDialog(screen4,"Вы уверены что хотите удалить Эмуляцию?","Удаление эмуляции",JOptionPane.YES_NO_OPTION);
-            if(res==JOptionPane.YES_OPTION){
+        if (((JButton) e.getSource()).getName().equals("but9delete")) {
+            int res = JOptionPane.showConfirmDialog((Component) screen4, "Вы уверены что хотите удалить Эмуляцию?", "Удаление эмуляции", JOptionPane.YES_NO_OPTION);
+            if (res == JOptionPane.YES_OPTION) {
                 //System.out.println();
-                if(l3.get(listObj.getSelectedIndex()).delete()){
+                if (l3.get(listObj.getSelectedIndex()).delete()) {
                     l3.remove(listObj.getSelectedIndex());
 //                    l.remove(listObj.getSelectedIndex());
                     l2.remove(listObj.getSelectedIndex());
                     listObj.setListData(l2);
                     hideEmButton();
-                }else{
-                    JOptionPane.showMessageDialog(screen4,"Не удалось удалить эмуляцию","Удаление эмуляции",JOptionPane.ERROR_MESSAGE);
+                } else {
+                    JOptionPane.showMessageDialog((Component) screen4, "Не удалось удалить эмуляцию", "Удаление эмуляции", JOptionPane.ERROR_MESSAGE);
                 }
             }
         }
     }
 
-    public void hideEmButton(){
+    public void hideEmButton() {
         but9delete.setEnabled(false);
         but8Correct.setEnabled(false);
         but4Play.setEnabled(false);
     }
 
-    public void showEmButton(){
+    public void showEmButton() {
         but9delete.setEnabled(true);
         but8Correct.setEnabled(true);
         but4Play.setEnabled(true);
     }
 
     public void saveObKeyPressed(ObKeyPressed obKeyPressed) {
-        if(obKeyPressed!=null){
-            if(saverFileemulation==null){
-                saverFileemulation=new JFileChooser();
+        if (obKeyPressed != null) {
+            if (saverFileemulation == null) {
+                saverFileemulation = new JFileChooser();
                 saverFileemulation.setCurrentDirectory(new File("./"));
                 saverFileemulation.setDialogTitle("Сохранение эмуляции");
                 saverFileemulation.setFileSelectionMode(JFileChooser.FILES_ONLY);
                 saverFileemulation.addChoosableFileFilter(new FileFilter() {
                     @Override
                     public boolean accept(File f) {
-                        if(f.isDirectory()) return true;
+                        if (f.isDirectory()) return true;
                         return f.getName().endsWith("emu");
                     }
 
@@ -333,31 +333,31 @@ public class PanelToolsEmulation extends JToolBar implements FocusListener, Acti
                     }
                 });
             }
-            int res=saverFileemulation.showSaveDialog(screen4);
-            if(res==JFileChooser.APPROVE_OPTION){
-                File file=saverFileemulation.getSelectedFile();
-                if(!file.getName().endsWith(".emu")){
-                    file=new File(file.getParentFile(),file.getName()+".emu");
+            int res = saverFileemulation.showSaveDialog((Component) screen4);
+            if (res == JFileChooser.APPROVE_OPTION) {
+                File file = saverFileemulation.getSelectedFile();
+                if (!file.getName().endsWith(".emu")) {
+                    file = new File(file.getParentFile(), file.getName() + ".emu");
                 }
-                screen4.fo.saveFile(file,obKeyPressed);
+                screen4.getFileOperation().saveFile(file, obKeyPressed);
 
-                JOptionPane.showMessageDialog(screen4,"Файл сохранен");
+                JOptionPane.showMessageDialog((Component) screen4, "Файл сохранен");
             }
         }
     }
 
     @Override
     public void fireStopped(final GlobalKeyListenerExample gkl) {
-        dialogCrEm=new DialogChKeyPr(screen4,new ObKeyPressed(gkl.getListKeyPressed()));
+        dialogCrEm = new DialogChKeyPr((Frame) screen4, new ObKeyPressed(gkl.getListKeyPressed()));
         dialogCrEm.setVisible(true);
-        if(screen4.keySt!=null){
+        if (screen4.getKeySt() != null) {
             but5Rec.setBorder(but6Folder.getBorder());
-            screen4.keySt=null;
+            screen4.setKeySt( null);
             but7Stop.setEnabled(false);
         }
     }
 
-    public Vector getAllObjEmulation(){
+    public Vector getAllObjEmulation() {
         return l2;
     }
 }
